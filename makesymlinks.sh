23afa223e17ee696ba6e667da9a51a6ee1dbd3a4
William Burns <mudokonman@gmail.com>
2014-11-04 10:47:03 -0500

    Ignore profile and bashrc for now

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bash_aliases bashrc profile gvimrc vimrc gitconfig"    # list of files/folders to symlink in homedir
+files="bash_aliases gvimrc vimrc gitconfig"    # list of files/folders to symlink in homedir
 
 ##########
 
